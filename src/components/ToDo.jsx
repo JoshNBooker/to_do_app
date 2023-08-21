3635a1e348c41bb5dc006ebd59ce0eefee2d86fd
Josh Booker <josh.booker2@gmail.com>
2023-08-21 18:02:13 +0100

    added radio buttons for to do priority

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,7 +2,8 @@ function ToDo({toDo}){
 
     return (
         <li className="toDo">
-            <h3>{toDo.description}</h3>
+            <h3><b>Task: </b>{toDo.description}</h3>
+            <h3><b>Priority:</b>{toDo.priority}</h3>
         </li>
     )
 }
--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -3,11 +3,13 @@ import ToDoList from "./ToDoList";
 
 function ToDoForm ({toDos,setToDos}) {
     const [description, setDescription] = useState("")
+    const [priority, setPriority] = useState("")
 
     const createToDo = () => {
         const newToDo = {
             id: ToDoList.length + 1,
-            description : description,
+            description: description,
+            priority: priority
         };
         setToDos([newToDo,...toDos])
         setDescription("")
@@ -17,8 +19,13 @@ function ToDoForm ({toDos,setToDos}) {
         <>
             <h3>Add To Do</h3>
             <div>
-                <label htmlFor="Description">Description</label>
+                <label htmlFor="Description">Description: </label>
                 <textarea name="Description" id="Description" cols="30" rows="10" onChange={(e) => setDescription(e.target.value)} value={description}></textarea>
+                <p>Select Priority:</p>
+                <label htmlFor="high">High</label>
+                <input type="radio" name="priority" id="high" value='high' onClick={(e) => setPriority(e.target.value)}/>
+                <label htmlFor="low">Low</label>
+                <input type="radio" name="priority" id="low" value='low' onClick={(e) => setPriority(e.target.value)}/>
             </div>
             <button onClick={createToDo}>Create To Do</button>
         </>
